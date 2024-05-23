5154d18f914d1d256a6b9f740ec88c9a7957f519
Ilya Shalonik <slon2895@gmail.com>
2024-05-23 14:29:59 +0200

    first commit

---
+++ src/input-output-types/video-types.ts
@@ -0,0 +1,45 @@
+export enum Resolutions {
+    P144 = 'P144',
+    P240 = 'P240',
+    P360 = 'P360',
+    P480 = 'P480',
+    P720 = 'P720',
+    P1080 = 'P1080',
+    P1440 = 'P1440',
+    P2160 = 'P2160',
+}
+
+export type ResolutionString = keyof typeof Resolutions
+
+export type OutputVideoType = {
+    id: number
+    title: string
+    author: string
+    canBeDownload: boolean
+    minAgeRestriction: null
+    createdAt: string
+    publicationDate: string
+    availableResolutions: Resolutions[]
+}
+
+export type InputVideoType = {
+    title: string
+    author: string
+    availableResolutions: Resolutions[]
+
+}
+
+export type ErrorType = {
+    field: string,
+    message: string
+}
+export type OutputErrorsType = {
+    errorsMessages: ErrorType[]
+}
+
+export enum CodeResponsesEnum {
+    Incorrect_values_400 = 400,
+    Not_found_404 = 404,
+    Not_content_204=204,
+
+}
